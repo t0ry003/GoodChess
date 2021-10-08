162c814a341ce5b942a0d267b654f0a0a54bf008
tory <rarescolteanu@gmail.com>
2021-10-08 15:27:43 +0300

    Initial commit

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,57 @@
+"""
+Engine Script
+"""
+
+
+class GameState():
+    def __init__(self):
+        """
+        tabla: 8x8, lista 2d;
+        primul caracter din fiecare string reprezinta culoarea (w -> white, b -> black);
+        al doilea caracter din fiecare string reprezinta numele piesei de sah:
+            ( K -> king, Q -> queen, R -> rook, P -> pawn, B -> bishop, N -> knight)
+        """
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
+        self.whiteToMove = True
+        self.moveLog = []
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove # pastreaza randul jucatorilor
+
+class Move():
+    # Sursa: https://www.dummies.com/games/chess/naming-ranks-and-files-in-chess/
+    # Tabla: https://www.dummies.com/wp-content/uploads/281905.image0.jpg
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    # reverse key in value
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    # reverse key in value
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChassNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
---
+++ Chess/ChessMain.py
@@ -0,0 +1,95 @@
+"""
+Main Script
+"""
+
+import pygame as p
+from Chess import ChessEngine
+
+# pentru o rezolutie optima am ales 512x512 (imaginile pieselor nu au o calitate foarte bune
+WIDTH = 512
+HEIGHT = 512
+# tabla este de tipul 8x8
+DIMENSION = 8
+# marimea unui patrat
+SQ_SIZE = HEIGHT // DIMENSION
+# numarul de fps-uri la care ruleaza jocul
+MAX_FPS = 15
+# pentru a adauga seturi de saj (skins)
+IMAGES = {}
+
+
+# incarcarea imaginilor intr-o lista IMAGES["initialaPiesa"]
+def loadImages():
+    pieces = ["wP", "wR", "wN", "wB", "wQ", "wK", "bP", "bR", "bN", "bB", "bQ", "bK"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+# functia principala(main)
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    # importam imaginile o singura data
+    loadImages()
+    running = True
+    sqSelected = ()  # nu este selectat nimic ((row, col))
+    playerClicks = []  # memoria click-urilor(mutarilor). Ex: [(6,4), (4,4)]
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # pozitia (x,y)
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                # pentru a nu muta in acelasi loc
+                if sqSelected == (row, col):
+                    sqSelected = ()  # reset
+                    playerClicks = []  # reset
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)  # pentru primul si al doilea click
+                if len(playerClicks) == 2:  # dupa al doilea click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    # DEBUG PRINT
+                    print("Mutare: " + move.getChassNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()  # reset
+                    playerClicks = []  # reset
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+# pentru a adauga seturi de sah (skins)
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+
+# functia care afiseaza tabla de sah (drawBoard())
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+# functia care afiseaza piesele in forma tablei de sah (drawPieces())
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            # verificam daca nu este log gol (notat in lista ca "--")
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+# apelarea functiei principale
+if __name__ == "__main__":
+    main()
