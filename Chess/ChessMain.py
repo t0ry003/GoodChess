bb9139381783f01a45f45a5fade4ec8d9e58caed
Rares Cristian Olteanu <rares.olteanu@cnodobescu.ro>
2022-01-05 17:39:14 +0200

    Added:  -> Move animation  -> Game Over Text  -> Reset board on "R" key
     Fixed:
      -> Slow render on some computers
      -> Bad unreleased animation on undo
      -> White border on bottom and right sides

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -5,8 +5,8 @@ Main Script
 import pygame as p
 import ChessEngine
 
-WIDTH = 785
-HEIGHT = 785
+WIDTH = 784
+HEIGHT = 784
 # tabla este de tipul 8x8
 DIMENSION = 8
 # marimea unui patrat
@@ -17,6 +17,8 @@ MAX_FPS = 15
 IMAGES = {}
 # window icon
 WINDOW_ICON = p.image.load('images/icon.png')
+# animate
+animate = True
 
 
 # incarcarea imaginilor intr-o lista IMAGES["culoarePiesa"]
@@ -41,6 +43,7 @@ def main():
     running = True
     sqSelected = ()  # nu este selectat nimic ((row, col))
     playerClicks = []  # memoria click-urilor(mutarilor). Ex: [(6,4), (4,4)]
+    gameOver = False
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
@@ -48,27 +51,29 @@ def main():
 
             # MOUSE BINDS
             elif e.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos()  # pozitia (x,y)
-                col = location[0] // SQ_SIZE
-                row = location[1] // SQ_SIZE
-                # pentru a nu muta in acelasi loc
-                if sqSelected == (row, col):
-                    sqSelected = ()  # reset
-                    playerClicks = []  # reset
-                else:
-                    sqSelected = (row, col)
-                    playerClicks.append(sqSelected)  # pentru primul si al doilea click
-                if len(playerClicks) == 2:  # dupa al doilea click
-                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
-                    # DEBUG PRINT
-                    # print("Mutare: " + move.getChassNotation())
-                    if move in validMoves:
-                        gs.makeMove(move)
-                        moveMade = True
+                if not gameOver:
+                    location = p.mouse.get_pos()  # pozitia (x,y)
+                    col = location[0] // SQ_SIZE
+                    row = location[1] // SQ_SIZE
+                    # pentru a nu muta in acelasi loc
+                    if sqSelected == (row, col):
                         sqSelected = ()  # reset
-                        playerClicks = []
+                        playerClicks = []  # reset
                     else:
-                        playerClicks = [sqSelected]
+                        sqSelected = (row, col)
+                        playerClicks.append(sqSelected)  # pentru primul si al doilea click
+                    if len(playerClicks) == 2:  # dupa al doilea click
+                        move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                        # DEBUG PRINT
+                        # print("Mutare: " + move.getChassNotation())
+                        if move in validMoves:
+                            gs.makeMove(move)
+                            moveMade = True
+                            animate = True
+                            sqSelected = ()  # reset
+                            playerClicks = []
+                        else:
+                            playerClicks = [sqSelected]
 
 
             # KEY BINDS
@@ -76,11 +81,36 @@ def main():
                 if e.key == p.K_z:
                     gs.undoMove()
                     moveMade = True
+                    animate = False
+                    gameOver = False
+                if e.key == p.K_r:  # reset the board
+                    gs = ChessEngine.GameState()
+                    validMoves = gs.getValidMoves()
+                    sqSelected = ()
+                    playerClicks = []
+                    moveMade = False
+                    animate = False
+                    gameOver = False
 
         if moveMade:
+            if animate:
+                animateMove(gs.moveLog[-1], screen, gs.board, clock)
             validMoves = gs.getValidMoves()
             moveMade = False
+            animate = False
+
         drawGameState(screen, gs, validMoves, sqSelected)
+
+        if gs.checkMate:
+            gameOver = True
+            if gs.whiteToMove:
+                drawText(screen, 'Black wins by checkmate')
+            else:
+                drawText(screen, 'Black wins by stalemate')
+        elif gs.staleMate:
+            gameOver = True
+            drawText(screen, 'Stalemate')
+
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -110,6 +140,7 @@ def drawGameState(screen, gs, validMoves, sqSelected):
 # functia care afiseaza tabla de sah (drawBoard())
 def drawBoard(screen):
     # BOARD COLORS
+    global colors
     colors = [p.Color(240, 217, 181), p.Color(181, 136, 99)]
     # END BOARD COLORS
 
@@ -124,9 +155,42 @@ def drawPieces(screen, board):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             piece = board[r][c]
-            # verificam daca nu este log gol (notat in lista ca "--")
+            # verificam daca nu este loc gol (notat in lista ca "--")
             if piece != "--":
                 screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+# functia de animare a mutarii
+def animateMove(move, screen, board, clock):
+    global colors
+    dR = move.endRow - move.startRow
+    dC = move.endCol - move.startCol
+    framesPerSquare = 10  # pentru un patrat
+    frameCount = (abs(dR) + abs(dC)) * framesPerSquare
+    for frame in range(frameCount + 1):
+        r, c = (move.startRow + dR * frame / frameCount, move.startCol + dC * frame / frameCount)
+        drawBoard(screen)
+        drawPieces(screen, board)
+        color = colors[(move.endRow + move.endCol) % 2]
+        endSquare = p.Rect(move.endCol * SQ_SIZE, move.endRow * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+        p.draw.rect(screen, color, endSquare)
+        # draw capture piece
+        if move.pieceCaptured != '--':
+            screen.blit(IMAGES[move.pieceCaptured], endSquare)
+        # draw moving piece
+        screen.blit(IMAGES[move.pieceMoved], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+        p.display.flip()
+        clock.tick(60)  # frame count for animation
+
+
+def drawText(screen, text):
+    font = p.font.SysFont("Helvitca", 45, True, False)
+    textObject = font.render(text, 0, p.Color('Gray'))
+    textLocation = p.Rect(0, 0, WIDTH, HEIGHT).move(WIDTH / 2 - textObject.get_width() / 2,
+                                                    HEIGHT / 2 - textObject.get_height() / 2)
+    screen.blit(textObject, textLocation)
+    textObject = font.render(text, 0, p.Color('Black'))
+    screen.blit(textObject, textLocation.move(2, 2))
 
 
 # apelarea functiei principale
